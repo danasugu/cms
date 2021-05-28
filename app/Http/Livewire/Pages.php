7e264857df1e588e2ec83f21e1c8045aa8305d78
Dana Sugu <dana.sugu@gmail.com>
2021-05-28 23:20:46 +0300

    Pages update

--- app/Http/Livewire/Pages.php
+++ app/Http/Livewire/Pages.php
@@ -2,10 +2,14 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Page;
 use Livewire\Component;
 
 class Pages extends Component
 {
+    public $slug;
+    public $title;
+    public $content;
     public function render()
     {
         return view('livewire.pages');
--- resources/views/livewire/pages.blade.php
+++ resources/views/livewire/pages.blade.php
@@ -1,3 +1,5 @@
-<div>
-    {{-- To attain knowledge, add things every day; To attain wisdom, subtract things every day. --}}
+<div class="p-6">
+      <x-jet-button wire:click="createShowModal">
+      {{ __('Create') }}
+      </x-jet-button>
 </div>
