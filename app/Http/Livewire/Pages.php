4f157e36a3436d6074f21a7d68438a3103cbd962
Dana Sugu <dana.sugu@gmail.com>
2021-05-29 10:49:56 +0300

    create page

--- app/Http/Livewire/Pages.php
+++ app/Http/Livewire/Pages.php
@@ -7,9 +7,26 @@ use Livewire\Component;
 
 class Pages extends Component
 {
+    public $modalFormVisible = true;
     public $slug;
     public $title;
     public $content;
+    
+    /**
+     * show the form modal of the create function
+     *
+     * @return void
+     */
+    public function createShowModal () 
+    {
+        $this-> modalFormVisible = true;
+    }
+    
+    /**
+     * the livewire render function
+     *
+     * @return void
+     */
     public function render()
     {
         return view('livewire.pages');
--- resources/views/livewire/pages.blade.php
+++ resources/views/livewire/pages.blade.php
@@ -1,5 +1,39 @@
 <div class="p-6">
+     <div class="flex items-center justify-end px-4 py-3 text-right sm:px-6">
+      
       <x-jet-button wire:click="createShowModal">
-      {{ __('Create') }}
+            {{ __('Create') }}
       </x-jet-button>
+      
+      </din>
+
+      {{-- modal form --}}
+
+<x-jet-dialog-modal wire:model="confirmingUserDeletion">
+            <x-slot name="title">
+
+{{ __('Save Page') }}
+            </x-slot>
+
+            <x-slot name="content">
+            <div class="block mt-4">
+            <x-jet-label for="title" value="{{ __('Title') }}" />
+            <x-jet-input id="title" class="block mt-1 w-full" type="text" wire:model.debounce.800ms="title"/>
+
+                  <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
+            </div>
+            </x-slot>
+
+            <x-slot name="footer">
+            <x-jet-secondary-button wire:click="$toggle('modalFormVisible')" wire:loading.attr="disabled">
+                  {{ __('Nevermind') }}
+            </x-jet-secondary-button>
+
+            <x-jet-button class="ml-2" wire:click="create" wire:loading.attr="disabled">
+                  {{ __('Save') }}
+            </x-jet-button>
+            </x-slot>
+            </x-jet-dialog-modal>
+
+
 </div>
